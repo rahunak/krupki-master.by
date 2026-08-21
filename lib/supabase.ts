7d95003d39fee3166555f2cf38477c1dcb55c4c2
Eugene Zaiko <66bratz@gmail.com>
2026-08-21 15:43:03 +0300

    fix: updated supabase url varialbe

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = process.env.SUPABASE_URL || 'https://krupki-master.by';;
 const supabaseAnonKey = process.env.SUPABASE_PUBLISHABLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
