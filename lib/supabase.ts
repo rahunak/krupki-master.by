b97173e92af73aca16c4ae3ef4f62914128fb8ff
Eugene Zaiko <66bratz@gmail.com>
2026-08-21 16:37:42 +0300

    fix: supabase url

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://krupki-master.by';;
+const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseAnonKey = process.env.SUPABASE_PUBLISHABLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
