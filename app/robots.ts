6577b0d480a82ce5ff99a040e65263c9d8b160f3
Eugene Zaiko <66bratz@gmail.com>
2026-08-22 22:56:25 +0300

    fix: updated robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,12 +6,12 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/admin/"],
+        disallow: ['/_next/', '/api/', '/admin/'],
       },
       {
         userAgent: "Yandex",
         allow: "/",
-        disallow: ["/api/", "/admin/"],
+        disallow: ['/_next/', '/api/', '/admin/'],
       },
       {
         userAgent: "GPTBot",
